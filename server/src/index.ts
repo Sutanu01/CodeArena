a68b882c5a49717457abdbd74a3ed75f2e13a66d
Akash Shaw <shawa0439@gmail.com>
2025-06-16 01:37:23 +0530

    render port fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -46,7 +46,7 @@ app.use('/api/cf', CodeForcesRoute)
 app.use('/api/features', Features)
 
 
-
-app.listen(5000, () => {
-    console.log("Server is running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log("Server is running on port ${PORT}");
 });
